a52b5897f362e7051edbd2959efbf5f151362cf9
Kristina Tuzova <ktuzova1901@gmail.com>
2022-12-24 20:00:18 +0200

    created slider

---
+++ src/js/slider.js
@@ -0,0 +1,46 @@
+let counter = 1;
+let nexbutton = document.querySelector('.swiper-next');
+let prevbutton = document.querySelector('.swiper-prev');
+let counterText = document.querySelector('.feedback__counter');
+if (counter === 1) {
+  prevbutton.classList.remove('.swiper-prev');
+  prevbutton.classList.add('swiper-disabled');
+}
+nexbutton.addEventListener('click', function () {
+   if (counter < 4) {
+    counter += 1;
+    counterText.textContent = `0${counter}/04`;
+  }
+  if (counter === 4) {
+    nexbutton.classList.remove('swiper-next');
+    nexbutton.classList.add('swiper-disabled');
+  }
+  if (counter === 1) {
+    prevbutton.classList.add('swiper-disabled');
+  }
+  if (counter > 1) {
+    prevbutton.classList.remove('swiper-disabled');
+  }
+});
+prevbutton.addEventListener('click', function () {
+  console.log(counter)
+  if (counter > 1) {
+    counter -= 1;
+    counterText.textContent = `0${counter}/04`;
+    nexbutton.classList.remove('swiper-disabled');
+    nexbutton.classList.add('swiper-next');
+  }
+  if (counter === 1) {
+    prevbutton.classList.add('swiper-disabled');
+  }
+});
+
+ 
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl:  '.swiper-prev',
+  }
+});
+
