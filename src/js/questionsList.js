c26fbf8a07313c21f793c90a99a59632e92636f8
Kristina Tuzova <ktuzova1901@gmail.com>
2022-12-25 13:46:19 +0200

    created component questions

---
+++ src/js/questionsList.js
@@ -0,0 +1,78 @@
+let arrayResponses = Array.from(
+  document.querySelectorAll(
+    '.questions__list-item-response'
+  )
+);
+let arrayButtons =
+  document.getElementsByClassName(
+    'questions__list-buttons'
+  );
+let arrayButtonsClose =
+  document.getElementsByClassName(
+    'questions__list-item-response-button'
+  );
+let arrayResponsesActive = [];
+for (let i = 0; i < arrayButtons.length; i++) {
+  arrayButtons[i].addEventListener(
+    'click',
+    function () {
+      arrayResponses.find((item, index) => {
+        if (i === index) {
+          item.classList.remove(
+            'questions__list-item-response'
+          );
+          item.classList.add(
+            'questions__list-item-response-visible'
+          );
+          arrayResponsesActive.push(item);
+        }
+        if (
+          index === i &&
+          arrayResponsesActive.length === 2
+        ) {
+          arrayResponsesActive[0].classList.remove(
+            'questions__list-item-response-visible'
+          );
+          arrayResponsesActive[0].classList.add(
+            'questions__list-item-response'
+          );
+          arrayResponsesActive.shift(item);
+        }
+      });
+    }
+  );
+}
+for (
+  let i = 0;
+  i < arrayButtonsClose.length;
+  i++
+) {
+  arrayButtonsClose[i].addEventListener(
+    'click',
+    function () {
+      arrayResponses.find((item, index) => {
+        if (i === index) {
+          item.classList.remove(
+            'questions__list-item-response'
+          );
+          item.classList.add(
+            'questions__list-item-response-visible'
+          );
+          arrayResponsesActive.push(item);
+        }
+        if (
+          index === i &&
+          arrayResponsesActive.length === 2
+        ) {
+          arrayResponsesActive[0].classList.remove(
+            'questions__list-item-response-visible'
+          );
+          arrayResponsesActive[0].classList.add(
+            'questions__list-item-response'
+          );
+          arrayResponsesActive.shift(item);
+        }
+      });
+    }
+  );
+}
