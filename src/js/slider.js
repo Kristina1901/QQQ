1af2321b1bc026bba0f0687a86950e22646710bf
Kristina Tuzova <ktuzova1901@gmail.com>
2022-12-26 11:09:19 +0200

    fixed cod

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,57 +1,64 @@
 let counter = 1;
-let nexbutton = document.querySelector('.swiper-next');
-let prevbutton = document.querySelector('.swiper-prev');
-let counterText = document.querySelector('.feedback__counter');
+let nexbutton = document.querySelector(
+  '.swiper-next'
+);
+let prevbutton = document.querySelector(
+  '.swiper-prev'
+);
+let counterText = document.querySelector(
+  '.feedback__counter'
+);
+let array = document.getElementsByClassName(
+  'swiper-slide'
+);
+console.log(array);
+
 if (counter === 1) {
   prevbutton.classList.remove('.swiper-prev');
   prevbutton.classList.add('swiper-disabled');
 }
 nexbutton.addEventListener('click', function () {
-   if (counter < 4) {
+  if (counter < 4) {
     counter += 1;
     counterText.textContent = `0${counter}/04`;
   }
   if (counter === 4) {
-    nexbutton.classList.remove('swiper-next');
-    nexbutton.classList.add('swiper-disabled');
+    nexbutton.setAttribute('disabled', true);
   }
   if (counter === 1) {
     prevbutton.classList.add('swiper-disabled');
   }
   if (counter > 1) {
-    prevbutton.classList.remove('swiper-disabled');
+    prevbutton.classList.remove(
+      'swiper-disabled'
+    );
   }
 });
 prevbutton.addEventListener('click', function () {
-  console.log(counter)
   if (counter > 1) {
     counter -= 1;
     counterText.textContent = `0${counter}/04`;
-    nexbutton.classList.remove('swiper-disabled');
+    nexbutton.removeAttribute('disabled');
     nexbutton.classList.add('swiper-next');
   }
   if (counter === 1) {
     prevbutton.classList.add('swiper-disabled');
   }
 });
-
 let swiper = new Swiper('.swiper', {
   rebuildOnUpdate: true,
+  updateOnWindowResize: true,
   observer: true,
+  grabCursor: true,
   navigation: {
     nextEl: '.swiper-next',
-    prevEl:  '.swiper-prev',
+    prevEl: '.swiper-prev',
   },
   breakpoints: {
-    768: {
-      allowSlidePrev: true,
-      allowSlideNext: true
-    },
+    768: {},
     320: {
       allowSlidePrev: true,
-      allowSlideNext: true
+      allowSlideNext: true,
     },
-  }
+  },
 });
-swiper.update();
-
